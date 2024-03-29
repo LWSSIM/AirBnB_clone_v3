875592f55edd27ff08a33aaa6c4f70478d791a60
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-29 22:13:11 +0000

    added app/v1/init&index blue print logic > ret(status:OK) json

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module for the API first endpoint """
+
+
+from flask import Flask
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+host = getenv('HBNB_API_HOST')
+port = getenv('HBNB_API_PORT')
+
+@app.teardown_appcontext
+def teardown(self):
+    """ Method to close the session """
+    from models import storage
+
+
+    storage.close()
+
+
+if __name__ == "__main__":
+    if host is None and port is None:
+        app.run(host='0.0.0.0', port=5000, threaded=True)
+    else:
+        app.run(host=host, port=port, threaded=True)
