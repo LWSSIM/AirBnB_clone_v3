5ef49d1dc5fa08596c1e6843320eb10c8ef910e1
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-29 22:44:43 +0000

    pcs app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from flask import Flask
 from api.v1.views import app_views
 from os import getenv
 
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
@@ -13,11 +14,11 @@ app.register_blueprint(app_views)
 host = getenv('HBNB_API_HOST')
 port = getenv('HBNB_API_PORT')
 
+
 @app.teardown_appcontext
 def teardown(self):
     """ Method to close the session """
     from models import storage
-
 
     storage.close()
 
